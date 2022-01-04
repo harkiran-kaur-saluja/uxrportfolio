c909491e2d574fd62d868115ea711e6ca1b5f106
harkisa9 <harkisa9@uw.edu>
2022-01-04 14:45:41 -0800

    add kidsteam images and fix some content

--- src/Assets/js/kidsteam.js
+++ src/Assets/js/kidsteam.js
@@ -6,15 +6,17 @@ export function KidsTeam () {
                 <h2 className="animate__animated animate__flipInX" >Kids<span className="firstName">team</span></h2>
                 <img className="px-5 center-img img-fluid" src="img/humaneOS/HumaneOS_banner.png" alt="a banner for humaneOS"/>
                 <h4>research overview</h4>
-                <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum scelerisque urna, sit amet ultricies odio iaculis quis. Nunc blandit a mauris sit amet porttitor. Aliquam erat volutpat. Donec nec congue leo, eget facilisis elit. Pellentesque vitae aliquam libero. Pellentesque turpis lacus, aliquet sit amet convallis ac, iaculis eget arcu. Aliquam pellentesque auctor mollis. Quisque ac dui libero.</h6>
+                <p>KidsTeam is an <strong className = "bolded-words">intergenerational co-design team</strong> that is comprised of children ages 7-11 years old, adult design partners, and sometimes various outside stakeholders (this can be industry partners such as Amazon, local partners such as the Seattle Public Library, or other universities such as USC). At KidsTeam, we engage in <strong className = "bolded-words">participatory design practices</strong> in which the children are equal stakeholders throughout the design process.</p>
+                <p>For two and a half years, I have been studying participatory design with children as part of KidsTeam UW under Professor Jason Yip. KidsTeam UW's research is part of the Information School (iSchool) at the University of Washington and we <strong className = "bolded-words">aim to understand the interactions with and perceptions of technology through co-design sessions, interviews, and qualitative coding of rich data</strong>, which further informs our approach to designing technologies with input from (and built for) children. I have worked on a variety of projects with several partners, including Amazon, Google, SunSmart, and other departments at UW.</p>
                 <ul>
-                    <li><p><strong>my role: </strong> Undergraduate Researcher</p></li>
-                    <li><p><strong>skills used: </strong> semi-structured contextual interviews, remote research, speed dating, participatory design, qualitative data analysis</p></li>
+                    <li><p><strong>my role: </strong> Undergraduate UX Research Assistant</p></li>
+                    <li><p><strong>skills used: </strong> semi-structured interviews, remote research, participatory design, qualitative data analysis, working with minors</p></li>
                     <li><p><strong>time spent conducting research: </strong>2.5 years</p></li>
+                    <li><p><strong>mentors: </strong>Jason Yip, Kung Jin Lee, Wendy Roldan</p></li>
                 </ul>
             </div>
                 <div className="px-5">
-                <h4>projects completed</h4>
+                <h4>projects I worked on</h4>
                 <h5>project1</h5>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum scelerisque urna, sit amet ultricies odio iaculis quis. Nunc blandit a mauris sit amet porttitor. Aliquam erat volutpat. Donec nec congue leo, eget facilisis elit. Pellentesque vitae aliquam libero. Pellentesque turpis lacus, aliquet sit amet convallis ac, iaculis eget arcu. Aliquam pellentesque auctor mollis. Quisque ac dui libero.</p>
                 <h5>project2</h5>
@@ -24,13 +26,13 @@ export function KidsTeam () {
             </div>
             <div className="px-5">
                 <h4>publications</h4>
-                <p>Lee, K.J., Roldan, W., Zhu, T.Q., Saluja, H.K., Na, S., Chin, B., Zeng, Y., Lee, J.H., &amp; Yip, J.C., (2021). The Show Must Go On: A conceptual model of conducting synchronous participatory design with children online. Proceedings of the 2021 CHI Conference on Human Factors in Computing Systems. Association for Computing Machinery.<br /><strong>Received Best Paper Award (Top 1% of 2,800 papers)</strong></p>
+                <p>Lee, K.J., Roldan, W., Zhu, T.Q., <strong className = "bolded-words">Saluja, H.K.</strong>, Na, S., Chin, B., Zeng, Y., Lee, J.H., &amp; Yip, J.C., (2021). The Show Must Go On: A conceptual model of conducting synchronous participatory design with children online. Proceedings of the 2021 CHI Conference on Human Factors in Computing Systems. Association for Computing Machinery.<br /><strong>Received Best Paper Award (Top 1% of 2,800 papers)</strong></p>
                 <br />
-                <p>Simko, L., Chin, B., Na, S., Saluja, H.K., Zhu, T.Q., Hiniker, A., Yip, J.C., &amp; Cobb, C. (2021). Would You Rather: A focus group method for eliciting and discussing formative design insights with children. Proceedings of the 2021 IDC Interaction Design and Children. Association for Computing Machinery.</p>
+                <p>Simko, L., Chin, B., Na, S., <strong className = "bolded-words">Saluja, H.K.</strong>, Zhu, T.Q., Hiniker, A., Yip, J.C., &amp; Cobb, C. (2021). Would You Rather: A focus group method for eliciting and discussing formative design insights with children. Proceedings of the 2021 IDC Interaction Design and Children. Association for Computing Machinery.</p>
             </div>
             <div className="px-5">
                 <h4>takeaways: what I learned</h4>
-                <p>description of what I learned by being a part of the kidsteam research experience</p>
+                <p>Prior to KidsTeam, I had no knowledge of the research process or even research in general. I had worked with children prior to this experience and was interested in HCI, hence my interest in Professor Yip’s work. Throughout the two and a half years, I got to understand the process and ethics of user research and work on various projects that resulted in me being the co-author of three academic papers as an undergraduate. It is important to give attention to participants in the co-design process, as they are crucial stakeholders. I also learned how to collect, process, and qualitatively analyze data, as well as conduct semi-structured interviews. Furthermore, I was able to grasp the literature review and paper writing process. Overall, this experience enabled me to pursue HCI education and UX research in the future and it set the foundation for my UX research career. </p>
             </div>
         </div>
       );
